ad457bd945f324b785701aa897a28833f6481f08
Alexmajak <alexmajak@mail.ru>
2025-04-05 16:14:09 +0500

    5/04/2025

---
+++ web-garage/1part/script.js
@@ -0,0 +1,14 @@
+let dayOfWeek = [
+	"Понедельник",
+	"Вторник",
+	"Среда",
+	"Четверг",
+	"Пятница",
+	"Суббота",
+	"Воскресенье",
+];
+
+for (let index = 0; index < dayOfWeek.length; index++) {
+	const element = dayOfWeek[index];
+	console.log(element);
+}
