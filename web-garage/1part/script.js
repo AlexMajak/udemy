03407805001aac6a311d7020bebf8debbe1adba3
Alexmajak <alexmajak@mail.ru>
2025-04-10 16:58:03 +0500

    10/04/2025

--- web-garage/1part/script.js
+++ web-garage/1part/script.js
@@ -1,14 +1,62 @@
-let dayOfWeek = [
-	"Понедельник",
-	"Вторник",
-	"Среда",
-	"Четверг",
-	"Пятница",
-	"Суббота",
-	"Воскресенье",
-];
+// const myCity = {
+// 	city: 'Tyumen',
+// 	info: {
+// 		isPopular: true,
+// 		country: 'russia'
+// 	}
+// }
 
-for (let index = 0; index < dayOfWeek.length; index++) {
-	const element = dayOfWeek[index];
-	console.log(element);
+
+// const name = 'Alex';
+// const age = 42;
+
+// const userProfile = {
+// 	// name: name,
+// 	// age: age
+// 	name,
+// 	age
+// }
+
+// console.log(userProfile);
+
+// const greeting = () => {
+// 	console.log('greeting');
+// }
+
+// // greeting();
+
+// const myCity1 = {
+// 	city: 'NewYork',
+// 	greeting
+// }
+
+// // console.log(myCity1);
+// myCity1.greeting();
+
+// let carJson = '{"color": "blue", "model": "lada","add": {	"have": true, "quantity": 5}}';
+
+// const myCityJson = JSON.stringify(myCity);
+// console.log(myCityJson);
+// const myCityObject = JSON.parse(myCityJson);
+// console.log(myCityObject)
+
+// console.log(carJson);
+// carJson = JSON.parse(carJson);
+// console.log(carJson);
+// console.log(carJson.model);
+
+const myCity = {
+	city: 'Tyumen',
+	info: {
+		isPopular: true,
+		country: 'russia'
+	}
 }
+
+const myCity2 = Object.assign({}, myCity);
+myCity2.river = 'Tura';
+myCity2.info.lake = true;
+console.log(myCity2);
+console.log(myCity);
+
+
